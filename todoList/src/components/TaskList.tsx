aba1bf47cea537bfccbb469e6977698a940aea7d
Leandro <leandro.souza@intmed.com.br>
2023-05-31 16:00:09 -0300

    feat: Custom checkbox done task

--- todoList/src/components/TaskList.tsx
+++ todoList/src/components/TaskList.tsx
@@ -1,6 +1,6 @@
 import { Trash } from 'phosphor-react'
 import style from './TaskList.module.css'
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 
 interface TaskProps {
@@ -10,25 +10,34 @@ interface TaskProps {
 }
 
 export function TaskList({task, onDeleteTask, onEditStatus}:TaskProps){
+    const [taskHasDone, setTaskHasDone] = useState(false)
     
     function handleDeleteTask(){
         onDeleteTask(task)
     }
 
     function handleEditStatusTask(event: ChangeEvent<HTMLInputElement>){
+        setTaskHasDone(event.target.checked)
         onEditStatus(event.target.checked);
+        console.log(taskHasDone);
+        
     }
 
     return(
         <div className={style.content}>
-            <span>
-                <input 
-                    type='checkbox' 
-                    className={style.checkboxRound}
-                    onChange={handleEditStatusTask} 
-                />
-                {task}
-            </span>
+            <div className={style.taskDescription}>
+                <label className={style.container}>
+                    <input 
+                        type='checkbox' 
+                        className={style.checkboxRound}
+                        onChange={handleEditStatusTask} 
+                    />
+                    <span className={style.checkmark}></span>
+                </label>
+                <span className={taskHasDone ? style.taskHasDone : style.taskNotDone}>
+                    {task}
+                </span>
+            </div>
             <button onClick={handleDeleteTask}>
                 <Trash size={20}/>
             </button>
